3d553766f5b55f902bb14d0673c750fad62253ae
Pedro Henrique <pedrohq238@gmail.com>
2025-02-21 17:43:40 -0300

    add lines button

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const { app, BrowserWindow } = require('electron');
-
 function createWindow () {
     const mainWindow = new BrowserWindow({
         width: 450,
@@ -11,15 +10,12 @@ function createWindow () {
 
     mainWindow.loadFile('src/index.html');
 }
-
 app.on('ready', createWindow);
-
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
         app.quit();
     }
 });
-
 app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) {
         createWindow();
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,24 +1,14 @@
 $(document).ready(function() {
     $("#addFieldButton").on("click", function(){
-        let fieldName = prompt({
-            title: 'Digite o nome do campo',
-            label: 'Nome:',
-            value: '',
-            inputAttrs: {
-                type: 'text'
-            },
-            type: 'input'
-          })
-          .then((r) => {
-            if (r === null) {
-                console.log('Usuário cancelou');
-            } else {
-                console.log('Input do usuário:', r);
-                // Aqui você pode enviar o valor para o renderer process, se necessário
-            }
-          })
-          .catch(console.error);
+        let line = `<div class="d-flex justify-content-center align-items-center w-100 mb-4">
+                        <div class="line d-flex justify-content-center align-items-center w-100">
+                            <label for="name" class="text-white me-2" style="width: 100px;">CAMPO</label>
+                            <input type="text" class="form-control me-2">
+                            <button class="btn btn-success me-2"><i class="bi bi-clipboard"></i></button>
+                            <button class="btn btn-danger"><i class="bi bi-trash"></i></button>
+                        </div>
+                    </div>`;
+        $("#form").append(line)
     });
-
-
-});como 
+    $("#deleteButton")
+});
